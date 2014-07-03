06fce3d8b2f716393e48e7e1a61934cf32f58b70
Hack Reactor Students <craftjk@gmail.com>
2014-07-03 16:54:23 -0700

    Added animations on link table

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,18 +1,28 @@
-angular.module('shortly.links', [])
+angular.module('shortly.links', ['ngAnimate', 'fx.animations'])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, $timeout, Links) {
 
   $scope.getLinks = Links.getLinks;
 
-  $scope.data = {};
+  $scope.data = {links: []};
 
-  $scope.getLinks() // todo: figure out why this isn't the same as $scope.data = Links.getLinks();
-    .then(function(links) {
-      $scope.data.links = links;
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+  $timeout(function(){
+    $scope.getLinks()
+      .then(function(links) {
+        links.sort(function(a,b) {
+          return b.visits - a.visits;
+        });
+
+        angular.forEach(links, function(url, idx) {
+          $timeout(function(){
+            $scope.data.links.push(url);
+          }, 100 * idx);
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+  }, 1);
 
   // $scope.data = $scope.getLinks();
   console.log($scope.data);
